7b4d2508653479dfb14b3cb5ea7b0c7ce59efedb
Tetyana Radu <tanyaradu@Tanyas-MBP.home>
2019-04-16 21:42:03 -0400

    logic added

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
 var score = 0
-
-var cnv = document.querySelector("#canvas")
-
+var missed = 0
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-setInterval(placeMole, 1800)
+var gameNav = {
+    startGame: function (evt) {
+        score = 0
+        missed = 0
+        gameNav.setIntData = setInterval(function () { placeMole(evt) }, 1000)
+    },
+    stopGame: function () {
+        clearInterval(gameNav.setIntData);
+    },
+    setIntData: 0
+}
 
-function placeMole(evt){
-    var mole = document.createElement("div")
+function placeMole(evt) {
+    var divCont = document.createElement("div")
+    divCont.setAttribute("class", "picCont")
+    divCont.setAttribute("data-id", "")
+    document.getElementById("gameContainer").appendChild(divCont)
+    var mole = document.createElement("img")
     mole.setAttribute("class", "molePic")
-    document.getElementById("gameContainer").appendChild(mole)
-    
+    mole.setAttribute("src", "mole.png")
+    divCont.appendChild(mole)
+
     var x = getRandomInt(0, 460)
     var y = getRandomInt(0, 260)
-    mole.style.left = `${x}px`
-    mole.style.top = `${y}px`
-    setTimeout(removeMole, 4000)
+    divCont.style.left = `${x}px`
+    divCont.style.top = `${y}px`
+    setTimeout(function () { removeMole(divCont) }, 3000)
 }
-document.getElementById("gameContainer").addEventListener("click", addScore)
-document.querySelector("#check").addEventListener("click", placeMole)
+document.querySelector("#gameContainer").addEventListener("click", addScore)
+document.querySelector("#start").addEventListener("click", gameNav.startGame)
+document.querySelector("#endGame").addEventListener("click", gameNav.stopGame)
 
-function removeMole(){
-    document.querySelectorAll(".molePic")[0].remove()
-   
+function removeMole(divCont) {
+    if (divCont.dataset.id == "") {
+        missed++
+        document.querySelector("#missed").innerHTML = `Missed ${missed}`
+    }
+    if (missed == 5) {
+        gameNav.stopGame()
+        alert("Game Over")
+    }
+    divCont.remove()
 }
-function removeMoleClick(event){
-    event.target.remove()
-   
-}
-function addScore(event){
+
+function addScore(event) {
     event.preventDefault()
-    removeMoleClick(event)
-    score ++
-    document.getElementById("score").innerHTML = `Score ${score}`
+    if (event.target.className == "divCont") {
+        event.target.setAttribute("data-id", "clicked")
+        event.target.remove()
+        score++
+        document.getElementById("score").innerHTML = `Score ${score}`
+    }
+    else if (event.target.className == "molePic") {
+        event.target.parentNode.setAttribute("data-id", "clicked")
+        event.target.parentNode.remove()
+        score++
+        document.getElementById("score").innerHTML = `Score ${score}`
+    }
 }
