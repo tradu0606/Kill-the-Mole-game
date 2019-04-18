d7d9f26ae4c3460246f1660fc0a9399d72001480
Tetyana Radu <tanyaradu@Tanyas-MBP.home>
2019-04-17 22:34:10 -0400

    style added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var score = 0
 var missed = 0
+var onOff = 0
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -7,12 +8,27 @@ function getRandomInt(min, max) {
 
 var gameNav = {
     startGame: function (evt) {
-        score = 0
-        missed = 0
+        if (onOff == 0) {
+            onOff = 1
+        } else {
+            gameNav.stopGame() 
+            onOff = 1 
+        }
         gameNav.setIntData = setInterval(function () { placeMole(evt) }, 1000)
+
     },
     stopGame: function () {
+        score = 0
+        missed = 0
+        onOff = 0
+        document.querySelector("#missed").innerHTML = `Missed ${missed}`
+        document.querySelector("#score").innerHTML = `Score ${score}`
         clearInterval(gameNav.setIntData);
+        var elemLeft = document.querySelectorAll(".picCont")
+        for (var i = 0; i < elemLeft.length; i++) {
+            elemLeft[i].remove()
+        }
+
     },
     setIntData: 0
 }
@@ -27,12 +43,13 @@ function placeMole(evt) {
     mole.setAttribute("src", "mole.png")
     divCont.appendChild(mole)
 
-    var x = getRandomInt(0, 460)
-    var y = getRandomInt(0, 260)
+    var x = getRandomInt(0, 630)
+    var y = getRandomInt(0, 330)
     divCont.style.left = `${x}px`
     divCont.style.top = `${y}px`
     setTimeout(function () { removeMole(divCont) }, 3000)
 }
+
 document.querySelector("#gameContainer").addEventListener("click", addScore)
 document.querySelector("#start").addEventListener("click", gameNav.startGame)
 document.querySelector("#endGame").addEventListener("click", gameNav.stopGame)
@@ -40,13 +57,18 @@ document.querySelector("#endGame").addEventListener("click", gameNav.stopGame)
 function removeMole(divCont) {
     if (divCont.dataset.id == "") {
         missed++
-        document.querySelector("#missed").innerHTML = `Missed ${missed}`
+        if (onOff == 1){
+            document.querySelector("#missed").innerHTML = `Missed ${missed}`
+        }
     }
     if (missed == 5) {
         gameNav.stopGame()
-        alert("Game Over")
+        gameOver()
+        score = 0
+        missed = 0
     }
     divCont.remove()
+
 }
 
 function addScore(event) {
@@ -62,5 +84,17 @@ function addScore(event) {
         event.target.parentNode.remove()
         score++
         document.getElementById("score").innerHTML = `Score ${score}`
+    }
+}
+
+function gameOver() {
+    if (missed >= 5) {
+        document.querySelector("#missed").innerHTML = `Missed 0`
+        document.querySelector("#score").innerHTML = `Score 0`
+        var gameOverMessage = document.createElement("h1")
+        document.querySelector("#gameContainer").appendChild(gameOverMessage)
+        gameOverMessage.setAttribute("id", "gameOverMess")
+        gameContainer.innerHTML = "GAME OVER"
+
     }
 }
