f7e7109a97b2e6b46a765b649d0d8d186c88fa98
Tetyana Radu <tanyaradu@Tanyas-MacBook-Pro.local>
2019-04-18 11:17:26 -0400

    version 1.0

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var score = 0
 var missed = 0
 var onOff = 0
+var timeMole = 1000
+var level = 0
+var messGameOver = `GAME OVER <br> Score ${score} <br> Lelel ${level}`
+var messCongr = `Congratulation! All moles are underground. Your score  ${score}`
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -14,26 +18,40 @@ var gameNav = {
             gameNav.stopGame() 
             onOff = 1 
         }
-        gameNav.setIntData = setInterval(function () { placeMole(evt) }, 1000)
-
+        if (document.querySelector("#gameOverMess")){
+            document.querySelector("#gameOverMess").remove()
+        }
+        
+        timeMole = 1000
+        gameNav.setIntData = setInterval(function () { placeMole() }, timeMole)
+        gameNav.setIntLevel = setInterval(function () { nextLevel() }, 10000)
     },
     stopGame: function () {
-        score = 0
-        missed = 0
-        onOff = 0
-        document.querySelector("#missed").innerHTML = `Missed ${missed}`
-        document.querySelector("#score").innerHTML = `Score ${score}`
-        clearInterval(gameNav.setIntData);
-        var elemLeft = document.querySelectorAll(".picCont")
-        for (var i = 0; i < elemLeft.length; i++) {
-            elemLeft[i].remove()
-        }
+        toZero()
+        stopGameMain()
 
     },
-    setIntData: 0
+    setIntData: 0,
+    setIntLevel: 0
+}
+function toZero(){
+    score = 0
+    missed = 0
+    onOff = 0
+    level = 0
+}
+function stopGameMain(){
+    document.querySelector("#missed").innerHTML = `Missed ${missed}`
+    document.querySelector("#score").innerHTML = `Score ${score}`
+    clearInterval(gameNav.setIntData);
+    clearInterval(gameNav.setIntLevel);
+    var elemLeft = document.querySelectorAll(".picCont")
+    for (var i = 0; i < elemLeft.length; i++) {
+        elemLeft[i].remove()
+    }
 }
 
-function placeMole(evt) {
+function placeMole() {
     var divCont = document.createElement("div")
     divCont.setAttribute("class", "picCont")
     divCont.setAttribute("data-id", "")
@@ -56,16 +74,16 @@ document.querySelector("#endGame").addEventListener("click", gameNav.stopGame)
 
 function removeMole(divCont) {
     if (divCont.dataset.id == "") {
-        missed++
         if (onOff == 1){
+            missed++
             document.querySelector("#missed").innerHTML = `Missed ${missed}`
         }
     }
     if (missed == 5) {
-        gameNav.stopGame()
-        gameOver()
-        score = 0
-        missed = 0
+        stopGameMain()
+        messGameOver
+        gameOver(messGameOver)
+        toZero()
     }
     divCont.remove()
 
@@ -87,14 +105,25 @@ function addScore(event) {
     }
 }
 
-function gameOver() {
-    if (missed >= 5) {
-        document.querySelector("#missed").innerHTML = `Missed 0`
-        document.querySelector("#score").innerHTML = `Score 0`
+function gameOver(message) {
         var gameOverMessage = document.createElement("h1")
         document.querySelector("#gameContainer").appendChild(gameOverMessage)
         gameOverMessage.setAttribute("id", "gameOverMess")
-        gameContainer.innerHTML = "GAME OVER"
+        gameOverMessage.innerHTML = message
+        document.querySelector("#missed").innerHTML = `Missed 0`
+        document.querySelector("#score").innerHTML = `Score 0`
+        document.querySelector("#level").innerHTML = `Level 0`
+        
+}
 
+function nextLevel(){
+    timeMole = timeMole - 50
+    if (timeMole == 0) {
+        gameOver(messCongr) 
+    } else{
+    level ++
+    document.querySelector("#level").innerHTML = `Level ${level}`
+    clearInterval(gameNav.setIntData);
+    gameNav.setIntData = setInterval(function () { placeMole() }, timeMole)
     }
 }
